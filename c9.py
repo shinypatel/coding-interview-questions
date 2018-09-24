d202d036946f2375242d1e36f52c4d0e82317c1c
shinypatel <patelshiny44@gmail.com>
2018-09-24 14:41:41 -0400

    c9 sorting algo

--- c9.py
+++ c9.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 def bubble_sort(arr):
     swapped = False
     for i in range(1, len(arr)):
@@ -9,9 +12,9 @@ def bubble_sort(arr):
     return arr
 
 
-def selection_sort(arr):
+def selection_sort(arr):    # outer loop - gives pos; inner loop - finds el
     n = len(arr)
-    for i in range(0, n):
+    for i in range(n):
         smallest, idx = float('inf'), None
         for j in range(i, n):
             if arr[j] < smallest:
@@ -35,7 +38,7 @@ def merge(arr1, arr2):
     return result + arr1[i:] + arr2[j:]
 
 
-def merge_sort(arr):
+def merge_sort(arr):    # sort in end
     n = len(arr)
     if n == 1:
         return arr
@@ -45,8 +48,38 @@ def merge_sort(arr):
     return merge(arr1, arr2)
 
 
+def quick_sort(arr):    # sort in beginning
+    n = len(arr)
+    if n == 1:
+        return arr
+    pivot = int(n / 2)
+    for i in range(pivot):
+        if arr[i] > arr[pivot]:
+            arr.append(arr.pop())
+    arr1 = quick_sort(arr[:pivot])
+    arr2 = quick_sort(arr[pivot:])
+    return arr1 + arr2
 
 
+def insertion_sort(arr):    # outer loop - gives el; inner loop - finds pos
+    n = len(arr)
+    for i in range(n):
+        for j in range(i):
+            if arr[j] > arr[i]:
+                arr.insert(j, arr.pop(i))
+    return arr
 
-a = [5, 4, 3, 2, 1]
-print(merge_sort(a))
+
+def bucket_sort(arr, m):
+    buckets = [[] for b in range(m)]
+
+    x_min, x_max = min(arr), max(arr)
+    interval = x_max - x_min
+    for x in arr:
+        idx = int(((x - x_min) / interval) * (m - 1))
+        buckets[idx].append(x)
+
+    result = []
+    for b in range(m):
+        result += insertion_sort(buckets[b])
+    return result
