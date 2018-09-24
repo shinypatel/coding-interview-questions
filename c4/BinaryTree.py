21868bbf3d8fb94a6af0f8e14f0d424407bb123c
shinypatel <patelshiny44@gmail.com>
2018-09-23 20:48:13 -0400

    c4 comp

--- c4/BinaryTree.py
+++ c4/BinaryTree.py
@@ -31,7 +31,7 @@ class Node:
         elif self.right:
             return self.right.node(v)
 
-    def in_order_successor(self, v):   # q5
+    def in_order_successor(self, v):  # q5
         node = self.node(v)
         successor = None
         if node.right:
@@ -48,26 +48,12 @@ class Node:
                 successor = parent.val
         return successor
 
-    def print_in_order(self):   # left -> root -> right
-        if self.left:
-            self.left.print_in_order()
-        print(self.val)
-        if self.right:
-            self.right.print_in_order()
-
-    def print_pre_order(self):      # root -> left -> right
+    def print_pre_order_traversal(self):  # root -> left -> right
         print(self.val)
         if self.left:
-            self.left.print_pre_order()
+            self.left.print_pre_order_traversal()
         if self.right:
-            self.right.print_pre_order()
-
-    def print_post_order(self):     # left -> right -> root
-        if self.left:
-            self.left.print_post_order()
-        if self.right:
-            self.right.print_post_order()
-        print(self.val)
+            self.right.print_pre_order_traversal()
 
 
 def size(node):
@@ -82,7 +68,7 @@ def height(node):
     return max(height(node.left), height(node.right)) + 1
 
 
-def balanced(node):     # q1
+def balanced(node):  # q1
     if not node:
         return True
     return balanced(node.left) and balanced(node.right) and \
@@ -90,13 +76,13 @@ def balanced(node):     # q1
 
 
 def median(length):
-    if length % 2 == 0:     # even
+    if length % 2 == 0:  # even
         return int(length / 2)
-    else:   # odd
+    else:  # odd
         return int(math.ceil(length / 2) - 1)
 
 
-def binary_tree(arr):   # q3
+def binary_tree(arr):  # q3
     length = len(arr)
     idx = median(length)
     root = Node(arr[idx])
@@ -119,7 +105,7 @@ def binary_tree(arr):   # q3
     https://stackoverflow.com/questions/2603692/what-is-the-difference-between-tree-depth-and-height '''
 
 
-def depth(node, d=-1):     # tail recursion
+def depth(node, d=-1):  # tail recursion
     if not node:
         return d
     d += 1
@@ -127,7 +113,7 @@ def depth(node, d=-1):     # tail recursion
 
 
 def depth_lists(q, depths):  # q4
-    d = []      # nodes at depth d
+    d = []  # nodes at depth d
     node = q.pop(0)
     while node:
         d.append(node.val)
@@ -138,11 +124,82 @@ def depth_lists(q, depths):  # q4
         node = q.pop(0)
 
     if len(q):
-        q.append(None)      # separate depths by None in queue
+        q.append(None)  # separate depths by None in queue
         depths = depth_lists(q, depths)
 
     depths.insert(0, d)
     return depths
+
+
+def in_order_traversal(node, order=None):  # left -> root -> right
+    if order is None:
+        order = []
+
+    if node.left:
+        order = in_order_traversal(node.left, order)
+    order.append(node.val)
+    if node.right:
+        order = in_order_traversal(node.right, order)
+    return order
+
+
+def post_order_traversal(node, order=None):  # left -> right -> root
+    if order is None:
+        order = []
+
+    if node.left:
+        order = post_order_traversal(node.left, order)
+    if node.right:
+        order = post_order_traversal(node.right, order)
+    order.append(node.val)
+    return order
+
+
+def least_common_ancestor(root, v1, v2):  # q6
+    in_order = in_order_traversal(root)
+    post_order = post_order_traversal(root)
+
+    i1, i2 = sorted([in_order.index(v1), in_order.index(v2)])
+    relatives = in_order[i1:i2 + 1]
+
+    for v in reversed(post_order):
+        if v in relatives:
+            return v
+
+
+def list2str(l):
+    return ''.join(str(el) for el in l)
+
+
+def sub_tree(t1, t2):  # q7
+    if list2str(in_order_traversal(t2)) in \
+            list2str(in_order_traversal(t1)):
+        if list2str(post_order_traversal(t2)) in \
+                list2str(post_order_traversal(t1)):
+            return True
+    return False
+
+
+def k_sum_path(node, k, path=None):     # q8
+    if path is None:
+        path = []
+        if node.left:
+            k_sum_path(node.left, k)
+        if node.right:
+            k_sum_path(node.right, k)
+
+    j = sum(path)
+    if j + node.val <= k:
+        path.append(node.val)
+        if k - (j + node.val):
+            if node.left:
+                # pass-by-value
+                # because default is pass-by-reference
+                k_sum_path(node.left, k, path[:])
+            if node.right:
+                k_sum_path(node.right, k, path[:])
+        else:
+            print(path)
 
 
 bt = Node(4)
@@ -152,4 +209,4 @@ bt.insert(1)
 bt.insert(5)
 bt.insert(2)
 
-print(depth_lists([bt, None], []))
+k_sum_path(bt, 4)
