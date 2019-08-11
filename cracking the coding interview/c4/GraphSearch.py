2ac1b72c4851d33389904931379cb332ac8c1759
shinypatel <patelshiny44@gmail.com>
2019-08-11 15:26:28 -0500

    fixed bfs

--- cracking the coding interview/c4/GraphSearch.py
+++ cracking the coding interview/c4/GraphSearch.py
@@ -5,6 +5,8 @@ adj_mat = [[0, 1, 1, 0],
            [0, 0, 0, 0]]
 visited = [0 for i in range(n)]
 
+queue = []
+
 
 def dfs(i):
     visited[i] = 1
@@ -15,27 +17,24 @@ def dfs(i):
 
 
 def run_dfs():  # q2
-    for i in range(1, 2):
+    for i in range(n):
         if not visited[i]:
             dfs(i)
 
 
 def bfs(i):
-    queue = []
+    visited[i] = 1
+    print(i)
     for j in range(n):
         if adj_mat[i][j] and not visited[j]:
-            visited[j] = 1
-            print(j)
             queue.append(j)
-    for j in queue:
-        bfs(j)
+    if queue:
+        bfs(queue.pop(0))
 
 
 def run_bfs():
     for i in range(n):
         if not visited[i]:
-            visited[i] = 1
-            print(i)
             bfs(i)
 
 
